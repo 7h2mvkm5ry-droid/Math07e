ef430b5023c57dde59625119b5ec2e9660cbc0e8
7h2mvkm5ry-droid <7h2mvkm5ry@privaterelay.appleid.com>
2026-06-16 11:16:15 +0200

    erste Übung eingebaut

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("MatheTrainer gestartet");
+function pruefen() {
+
+    const antwort =
+        Number(document.getElementById("antwort").value);
+
+    if (antwort === 17) {
+
+        document.getElementById("ergebnis").innerHTML =
+            "✅ Richtig!";
+
+    } else {
+
+        document.getElementById("ergebnis").innerHTML =
+            "❌ Leider falsch.";
+
+    }
+
+}
